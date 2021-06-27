1102901707ad437b2406289c5ec40c4c336f9cb0
Lautaro Coccia <lautaroauditore@gmail.com>
2021-06-27 20:41:46 -0300

    Update Matriz4x4

--- Assets/Scripts/Quaternion/Matriz4x4.cs
+++ Assets/Scripts/Quaternion/Matriz4x4.cs
@@ -3,110 +3,300 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public struct Matriz4x4 
+namespace CustomMath
 {
-    public float m00;
-    public float m33;
-    public float m23;
-    public float m13;
-    public float m03;
-    public float m32;
-    public float m22;
-    public float m02;
-    public float m12;
-    public float m21;
-    public float m11;
-    public float m01;
-    public float m30;
-    public float m20;
-    public float m10;
-    public float m31;
-
-    public Matriz4x4(Vector4 column0, Vector4 column1, Vector4 column2, Vector4 column3)
+    public struct Matriz4x4
     {
-        m00 = column0.x; m01 = column1.x; m02 = column2.x; m03 = column3.x;
-        m10 = column0.y; m11 = column1.y; m12 = column2.y; m13 = column3.y;
-        m20 = column0.z; m21 = column1.z; m22 = column2.z; m23 = column3.z;
-        m30 = column0.w; m31 = column1.w; m32 = column2.w; m33 = column3.w;
-    }
-    public float this[int index] 
-    { 
-        get 
-        {
-            switch(index)
-            {
-                case 0:
-                    return m00;
-                case 1:
-                    return m01;
-                case 2:
-                    return m02;
-                case 3:
-                    return m03;
-                case 4:
-                    return m10;
-                case 5:
-                    return m11;
-                case 6:     
-                    return m12;
-                case 7:     
-                    return m13;
-                case 8:
-                    return m20;
-                case 9:
-                    return m21;
-                case 10:
-                    return m22;
-                case 11:
-                    return m23;
-                case 12:
-                    return m30;
-                case 13:
-                    return m31;
-                case 14:
-                    return m32;
-                case 15:
-                    return m33;
-                default:
-                    throw new IndexOutOfRangeException("Invalid Index");
-            }
-            
-        } 
-        set; }
-    //public float this[int row, int column] { get; set; }
+        public float m00;
+        public float m33;
+        public float m23;
+        public float m13;
+        public float m03;
+        public float m32;
+        public float m22;
+        public float m02;
+        public float m12;
+        public float m21;
+        public float m11;
+        public float m01;
+        public float m30;
+        public float m20;
+        public float m10;
+        public float m31;
 
-    public static Matriz4x4 zero { get {
-            Vector4 vec1 = new Vector4(0f, 0f, 0f, 0f);
-            Vector4 vec2 = new Vector4(0f, 0f, 0f, 0f);
-            Vector4 vec3 = new Vector4(0f, 0f, 0f, 0f);
-            Vector4 vec4 = new Vector4(0f, 0f, 0f, 0f);
-            return new Matriz4x4(vec1, vec2, vec3, vec4);
+        public Matriz4x4(Vector4 column0, Vector4 column1, Vector4 column2, Vector4 column3)
+        {
+            m00 = column0.x; m01 = column1.x; m02 = column2.x; m03 = column3.x;
+            m10 = column0.y; m11 = column1.y; m12 = column2.y; m13 = column3.y;
+            m20 = column0.z; m21 = column1.z; m22 = column2.z; m23 = column3.z;
+            m30 = column0.w; m31 = column1.w; m32 = column2.w; m33 = column3.w;
+        }
+        public float this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return m00;
+                    case 1:
+                        return m01;
+                    case 2:
+                        return m02;
+                    case 3:
+                        return m03;
+                    case 4:
+                        return m10;
+                    case 5:
+                        return m11;
+                    case 6:
+                        return m12;
+                    case 7:
+                        return m13;
+                    case 8:
+                        return m20;
+                    case 9:
+                        return m21;
+                    case 10:
+                        return m22;
+                    case 11:
+                        return m23;
+                    case 12:
+                        return m30;
+                    case 13:
+                        return m31;
+                    case 14:
+                        return m32;
+                    case 15:
+                        return m33;
+                    default:
+                        throw new IndexOutOfRangeException("Invalid Index");
+                }
+
+            }
+            //Poner un setter
+        }
+        public float this[int row, int column] {
+            get
+            {
+                switch(row)
+                {
+                    case 0:
+                        switch(column)
+                        {
+                            case 0:
+                                return m00;
+                            case 1:
+                                return m01;
+                            case 2:
+                                return m02;
+                            case 3:
+                                return m03;
+                            default:
+                                throw new IndexOutOfRangeException("Invalid Index");
+                        }
+                    case 1:
+                        switch (column)
+                        {
+                            case 0:
+                                return m10;
+                            case 1:
+                                return m11;
+                            case 2:
+                                return m12;
+                            case 3:
+                                return m13;
+                            default:
+                                throw new IndexOutOfRangeException("Invalid Index");
+                        }
+                    case 2:
+                        switch (column)
+                        {
+                            case 0:
+                                return m20;
+                            case 1:
+                                return m21;
+                            case 2:
+                                return m22;
+                            case 3:
+                                return m23;
+                            default:
+                                throw new IndexOutOfRangeException("Invalid Index");
+                        }
+                    case 3:
+                        switch (column)
+                        {
+                            case 0:
+                                return m30;
+                            case 1:
+                                return m31;
+                            case 2:
+                                return m32;
+                            case 3:
+                                return m33;
+                            default:
+                                throw new IndexOutOfRangeException("Invalid Index");
+                        }
+                    default:
+                        throw new IndexOutOfRangeException("Invalid Index");
+                }
+            } 
+            //agregar setter 
+        }
+
+        public static Matriz4x4 zero
+        {
+            get
+            {
+                Vector4 vecX = new Vector4(0f, 0f, 0f, 0f);
+                Vector4 vecY = new Vector4(0f, 0f, 0f, 0f);
+                Vector4 vecZ = new Vector4(0f, 0f, 0f, 0f);
+                Vector4 vecW = new Vector4(0f, 0f, 0f, 0f);
+                return new Matriz4x4(vecX, vecY, vecZ, vecW);
+            }
+        }
+        public static Matriz4x4 identity
+        {
+            get
+            {
+                Vector4 vecX = new Vector4(1f, 0f, 0f, 0f);
+                Vector4 vecY = new Vector4(0f, 1f, 0f, 0f);
+                Vector4 vecZ = new Vector4(0f, 0f, 1f, 0f);
+                Vector4 vecW = new Vector4(0f, 0f, 0f, 1f);
+                return new Matriz4x4(vecX, vecY, vecZ, vecW);
+            }
+        }
+        public Matriz4x4 transpose
+        {
+            get
+            {
+                return Transpose(this);
+            }
+        }
+        /*public Quaternion rotation
+        {
+            get
+            {
+                Quaternion q = Quaternion.identity;
+            }
+        }*/
+        //public Vector3 lossyScale { get; }
+        
+        //public Matriz4x4 inverse { get; }
+        //public static Matriz4x4 Inverse(Matriz4x4 m);
+        public static Matriz4x4 Rotate(Quaternion q)
+        {
+            Matriz4x4 m = Matriz4x4.identity;
+            m.m00 = 1 - 2 * (q.y * q.y) - 2 * (q.z * q.z);
+            m.m01 = 2 * q.x * q.y - 2 * q.z * q.w;
+            m.m02 = 2 * q.x * q.z + 2 * q.y * q.w;
+            m.m10 = 2 * q.x * q.y + 2 * q.z * q.w;
+            m.m11 = 1 - 2 * (q.x * q.x) - 2 * (q.z * q.z);
+            m.m12 = 2 * q.y * q.z - 2 * q.x * q.w;
+            m.m20 = 2 * q.x * q.z - 2 * q.y * q.w;
+            m.m21 = 2 * q.y * q.z + 2 * q.x * q.w;
+            m.m22 = 1 - 2 * (q.x * q.x) - 2 * (q.y * q.y);
+            return m;
+        }
+        public static Matriz4x4 Scale(Vector3 vec3)
+        {
+            Matriz4x4 m = Matriz4x4.zero;
+            m.m00 = vec3.x;
+            m.m11 = vec3.y;
+            m.m22 = vec3.z;
+            m.m33 = 1;
+            return m;
+        }
+        public static Matriz4x4 Translate(Vector3 vec3)
+        {
+            Matriz4x4 m = Matriz4x4.identity;
+            m.m03 = vec3.x;
+            m.m13 = vec3.y;
+            m.m23 = vec3.z;
+            m.m33 = 1;
+            return m;
+        }
+        public static Matriz4x4 Transpose(Matriz4x4 m)
+        {
+            return new Matriz4x4(new Vector4(m.m00, m.m01, m.m02, m.m03), new Vector4(m.m10, m.m11, m.m12, m.m13),
+                                 new Vector4(m.m20, m.m21, m.m22, m.m23), new Vector4(m.m30, m.m31, m.m32, m.m33));
+        }
+        public static Matriz4x4 TRS(Vector3 pos, Quaternion q, Vector3 s)
+        {
+            Matriz4x4 translate = Matriz4x4.Translate(pos);
+            Matriz4x4 rotate = Matriz4x4.Rotate(q);
+            Matriz4x4 scale = Matriz4x4.Scale(s);
+                 
+            Matriz4x4 m = translate * rotate * scale;
+
+            return m;
+        }
+        public override bool Equals(object other)
+        {
+            if (!(other is Matriz4x4)) return false;
+            return Equals((Matriz4x4)other);
+        }
+        public bool Equals(Matriz4x4 other)
+        {
+            return this == other;
+
+        }
+        //public Vector4 GetColumn(int index);
+        //public Vector3 MultiplyPoint(Vector3 point);
+        //public Vector3 MultiplyPoint3x4(Vector3 point);
+        //public Vector3 MultiplyVector(Vector3 vector);
+        //public void SetColumn(int index, Vector4 column);
+        //public void SetRow(int index, Vector4 row);
+        //public void SetTRS(Vector3 pos, Quaternion q, Vector3 s);
+        public override string ToString()
+        {
+            return ("m00 " + m00 + " m01 " + m01 + " m02 " + m02 + " m03 " + m03 +
+                  "\nm10 " + m10 + " m11 " + m11 + " m12 " + m12 + " m13 " + m13 +
+                  "\nm20 " + m20 + " m21 " + m21 + " m22 " + m22 + " m23 " + m23 +
+                  "\nm30 " + m30 + " m31 " + m31 + " m32 " + m32 + " m33 " + m33);
+        }
+        public static Vector4 operator *(Matriz4x4 a, Vector4 vector)
+        {
+            Vector4 vec4 = Vector4.zero;
+            vec4.x = (a.m00 * vector.x) + (a.m01 * vector.x) + (a.m02 * vector.x) + (a.m03 * vector.x);
+            vec4.y = (a.m10 * vector.y) + (a.m11 * vector.y) + (a.m12 * vector.y) + (a.m13 * vector.y);
+            vec4.z = (a.m20 * vector.z) + (a.m21 * vector.z) + (a.m22 * vector.z) + (a.m23 * vector.z);
+            vec4.w = (a.m30 * vector.w) + (a.m31 * vector.w) + (a.m32 * vector.w) + (a.m33 * vector.w);
+            return vec4;
+        }
+        public static Matriz4x4 operator *(Matriz4x4 a, Matriz4x4 b)
+        {
+            Matriz4x4 m = Matriz4x4.zero;
+
+            m.m00 = (a.m00 * b.m00) + (a.m01 * b.m10) + (a.m02 * b.m20) + (a.m03 * b.m30);
+            m.m01 = (a.m00 * b.m01) + (a.m01 * b.m11) + (a.m02 * b.m21) + (a.m03 * b.m31);
+            m.m02 = (a.m00 * b.m02) + (a.m01 * b.m12) + (a.m02 * b.m22) + (a.m03 * b.m32);
+            m.m03 = (a.m00 * b.m03) + (a.m01 * b.m13) + (a.m02 * b.m23) + (a.m03 * b.m33);
+            
+            m.m10 = (a.m10 * b.m00) + (a.m11 * b.m10) + (a.m12 * b.m20) + (a.m13 * b.m30);
+            m.m11 = (a.m10 * b.m01) + (a.m11 * b.m11) + (a.m12 * b.m21) + (a.m13 * b.m31);
+            m.m12 = (a.m10 * b.m02) + (a.m11 * b.m12) + (a.m12 * b.m22) + (a.m13 * b.m32);
+            m.m13 = (a.m10 * b.m03) + (a.m11 * b.m13) + (a.m12 * b.m23) + (a.m13 * b.m33);
+            
+            m.m20 = (a.m20 * b.m00) + (a.m21 * b.m10) + (a.m22 * b.m20) + (a.m23 * b.m30);
+            m.m21 = (a.m20 * b.m01) + (a.m21 * b.m11) + (a.m22 * b.m21) + (a.m23 * b.m31);
+            m.m22 = (a.m20 * b.m02) + (a.m21 * b.m12) + (a.m22 * b.m22) + (a.m23 * b.m32);
+            m.m23 = (a.m20 * b.m03) + (a.m21 * b.m13) + (a.m22 * b.m23) + (a.m23 * b.m33);
+            
+            m.m30 = (a.m30 * b.m00) + (a.m31 * b.m10) + (a.m32 * b.m20) + (a.m33 * b.m30);
+            m.m31 = (a.m30 * b.m01) + (a.m31 * b.m11) + (a.m32 * b.m21) + (a.m33 * b.m31);
+            m.m32 = (a.m30 * b.m02) + (a.m31 * b.m12) + (a.m32 * b.m22) + (a.m33 * b.m32);
+            m.m33 = (a.m30 * b.m03) + (a.m31 * b.m13) + (a.m32 * b.m23) + (a.m33 * b.m33);
+            return m;
+        }
+        public static bool operator ==(Matriz4x4 a, Matriz4x4 b)
+        {
+            return (a == b);
+        }
+        public static bool operator !=(Matriz4x4 a, Matriz4x4 b)
+        {
+            return (a != b);
         }
     }
-    //public static Matriz4x4 identity { get; }
-    //public Matriz4x4 transpose { get; }
-    //public Quaternion rotation { get; }
-    //public Vector3 lossyScale { get; }
-    //
-    //public Matriz4x4 inverse { get; }
-    //public static Matriz4x4 Inverse(Matriz4x4 m);
-    //public static Matriz4x4 Rotate(Quaternion q);
-    //public static Matriz4x4 Scale(Vector3 vector);
-    //public static Matriz4x4 Translate(Vector3 vector);
-    //public static Matriz4x4 Transpose(Matriz4x4 m);
-    //public static Matriz4x4 TRS(Vector3 pos, Quaternion q, //Vector3 s);
-    //public override bool Equals(object other);
-    //public bool Equals(Matriz4x4 other);
-    //public Vector4 GetColumn(int index);
-    //public Vector3 MultiplyPoint(Vector3 point);
-    //public Vector3 MultiplyPoint3x4(Vector3 point);
-    //public Vector3 MultiplyVector(Vector3 vector);
-    //public void SetColumn(int index, Vector4 column);
-    //public void SetRow(int index, Vector4 row);
-    //public void SetTRS(Vector3 pos, Quaternion q, Vector3 s);
-    //public override string ToString();
-    //public static Vector4 operator *(Matriz4x4 lhs, Vector4 vector);
-    //public static Matriz4x4 operator *(Matriz4x4 lhs, Matriz4x4 rhs);
-    //public static bool operator ==(Matriz4x4 lhs, Matriz4x4 rhs);
-    //public static bool operator !=(Matriz4x4 lhs, Matriz4x4 rhs);
+
 }
